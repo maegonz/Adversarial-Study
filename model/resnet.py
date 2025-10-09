ea34cbc24d91258d5cd0446e87d8c917723e772e
Maegon <manutonymanu@gmail.com>
2025-10-09 21:11:59 +0200

    First version model (4 block)

--- model/resnet.py
+++ model/resnet.py
@@ -7,5 +7,38 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_channels = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
+        self.block1 = self._make_layer(ResBlock, 64, 2, stride=1)
+        self.block2 = self._make_layer(ResBlock, 128, 2, stride=2)
+        self.block3 = self._make_layer(ResBlock, 256, 2, stride=2)
+        self.block4 = self._make_layer(ResBlock, 512, 2, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, n_class)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.bn1(y)
+        y = self.relu(y)
+        y = self.maxpool(y)
+        
+        y = self.block1(y)
+        y = self.block2(y)
+        y = self.block3(y)
+        y = self.block4(y)
+        
+        y = self.avgpool(y)
+        y = y.view(y.size(0), -1)
+        y = self.fc(y)
+        return y        
