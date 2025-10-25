46941cd2a5b8ea78113f313e92c5db01cf126253
Maegon <manutonymanu@gmail.com>
2025-10-25 13:11:07 +0200

    Re write forward and __init__ methods and commented _weight_init method

--- model/resblock.py
+++ model/resblock.py
@@ -11,48 +11,46 @@ class ResBlock(nn.Module):
         
         super(ResBlock, self).__init__()
 
-        self.in_chnl = in_channels
-        self.out_chnl = out_channels
-        self.kernel = kernel_size
-        self.stride = stride
-
-        self.conv1 = nn.Conv2d(self.in_chnl, self.out_chnl, self.kernel, paddings = 1, stride = self.stride, biais=False)
-        self.bn1 = nn.BatchNorm2d(self.out_chnl)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, paddings = 1, stride = stride, biais=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(self.out_chnl, self.out_chnl, self.kernel, self.stride, padding=1, biais = False)
-        self.bn2 = nn.BatchNorm2d(self.out_chnl)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel = kernel_size, stride=1, padding=1, biais = False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         # self.dropout = nn.Dropout(p = self.dropout)
 
         self.shortcut = nn.Sequential()
-        if self.stride != 1 or self.in_chnl != self.out_chnl:
+        if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(self.in_chnl, self.out_chnl, kernel_size=1, stride=self.stride, biais=False),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, biais=False),
                 nn.BatchNorm2d(out_channels)
             )
 
         self._weight_init()
 
-    def _weight_init(self):
-        for module in self.modules():
-            if isinstance(module, nn.Conv2d):
-                # Do not initiialize biais, due to batchnorm
-                # if module.biais is not None:
-                #     nn.init.constant_(module.biais, 0)
-                nn.init.kaiming_normal_(module.weight)
+    # def _weight_init(self):
+    #     for module in self.modules():
+    #         if isinstance(module, nn.Conv2d):
+    #             # Do not initiialize biais, due to batchnorm
+    #             # if module.biais is not None:
+    #             #     nn.init.constant_(module.biais, 0)
+    #             nn.init.kaiming_normal_(module.weight)
 
-            elif isinstance(module, nn.BatchNorm2d):
-                # Standard initialization for batch normalization-
-                nn.init.constant_(module.weight, 1)
-                nn.init.constant_(module.bias, 0)
+    #         elif isinstance(module, nn.BatchNorm2d):
+    #             # Standard initialization for batch normalization-
+    #             nn.init.constant_(module.weight, 1)
+    #             nn.init.constant_(module.bias, 0)
 
-            elif isinstance(module, nn.Linear):
-                nn.init.kaiming_normal_(module.weight)
-                nn.init.constant_(module.bias, 0)             
+    #         elif isinstance(module, nn.Linear):
+    #             nn.init.kaiming_normal_(module.weight)
+    #             nn.init.constant_(module.bias, 0)             
 
     def forward(self, x):
-        y = self.relu(self.bn1(self.conv1(x)))
-        y = self.bn2(self.conv2(y))
+        y = self.conv1(x)
+        y = self.bn1(y)
+        y = self.relu(y)
+        y = self.conv2(y)
+        y = self.bn2(y)
         y += self.shortcut(x)
         y = self.relu(y)
         return y
