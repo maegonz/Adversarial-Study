1a8463b2e31ebc816ae874161c9b6ff91388a297
Maegon <manutonymanu@gmail.com>
2025-10-25 15:46:44 +0200

    Add docstring for evaulating method and add accuracy as an output

--- model/method.py
+++ model/method.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-def train(model,
+def training(model,
          train_set: DataLoader,
          criterion,
          optimizer,
@@ -44,19 +44,40 @@ def train(model,
         print(f"Epoch {epoch+1}/{epochs} - Train Loss: {epoch_loss:.4f}")
 
         if val_set is not None:
-            val_loss = evaluate(model, val_set, criterion, device)
+            val_loss = evaluating(model, val_set, criterion, device)
             print(f"Val Loss:   {val_loss:.4f}")
 
-def evaluate(model, 
-             data_set: DataLoader,
-             criterion,
-             device):
+def evaluating(model, 
+               data_set: DataLoader,
+               criterion,
+               device):
     """
     Evaluate the model on a dataset.
+
+    Params
+    -------
+    model: nn.Module 
+        Model to be evaluated.
+    data_set: DataLoader
+        Dataset or DataLoader to evaluate the model on.
+    criterion : nn.Module
+        Loss function used for evaluation.
+    device : torch.device or str
+        Device to perform evaluation on (e.g., 'cpu' or 'cuda').
+
+    Returns
+    -------
+    avg_loss : float
+        Average loss over the dataset.
+    avg_accuracy : float
+        Average accuracy (in percentage) over the dataset.
     """
 
     model.eval()
+
     total_loss = 0
+    accuracy = 0
+    total = 0
 
     with torch.no_grad():
         for inputs, labels in data_set:
@@ -64,5 +85,12 @@ def evaluate(model,
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             total_loss += loss.item() * inputs.size(0)
-    
-    return total_loss/len(data_set.dataset)
+
+            _, pred = torch.max(outputs, 1)
+            accuracy += (pred == labels).sum().item()
+            total += labels.size(0)
+
+    avg_loss = total_loss / total
+    avg_accuracy = 100 * (accuracy / total)
+
+    return avg_loss, avg_accuracy
