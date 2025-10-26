2629e23ed8e0bde326e292b2fe526ea718fe4a12
Maegon <manutonymanu@gmail.com>
2025-10-26 11:13:57 +0100

    Correct separation fucntion and adapt the docstring

--- data/dataprocess.py
+++ data/dataprocess.py
@@ -4,7 +4,8 @@ import torch
 from torch.utils.data import DataLoader
 
 def separation(dataset: DataLoader,
-               id_class: int):
+               id_class: int,
+               array: bool=False):
     """
     Extract all image tensors belonging to a specific class from a DataLoader.
 
@@ -15,19 +16,31 @@ def separation(dataset: DataLoader,
     id_class : int
         The class label used to filter the dataset. Only images with this label 
         will be included in the output.
+    array: bool
+        Whether or not the givent function return an np.array(set to True) or a torch.tensor(set to False).
+        Defaults to False.
+
     Returns
     -------
-    x:  np.ndarray
-        A concatenated array containing all images from the specified class.
+    x:  np.ndarray or torch.tensor
+        A concatenated array or tensor containing all images from the specified class.
     """
 
     x = []
 
     for batch in dataset:
-        mask = batch[1] == id_class
-        images = batch[0][mask]
-        x.append(images)
+        images, labels = batch[0], batch[1]
+        mask = labels == id_class
+        selected_images = images[mask]
+        x.append(selected_images)
 
-    x = np.concatenate(x, axis=0)
+    # Concatenate all selected images into one tensor
+    x = torch.cat(x, dim=0)
+    # Turn the tensor into a np.array if array is True
+    if array:
+        try:
+            x.detach().cpu().numpy()
+        except:
+            x = np.array(x)
 
     return x
