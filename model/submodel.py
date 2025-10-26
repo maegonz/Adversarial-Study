b2470b86d8348471875dcf125c737e63435658bb
Maegon <manutonymanu@gmail.com>
2025-10-26 10:28:12 +0100

    Define split function

---
+++ model/submodel.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from typing import Optional
+
+def split(model: nn.Module,
+          id: Optional[int] = None):
+    """
+    Split a model in two, defaults to split at the penultimate layer.
+
+    Params
+    -------
+    model: nn.Module
+        Model to split.
+    id: int, Optional
+        Index of the layer split point. Defaults to the penultimate layer.
+
+    Returns
+    -------
+    g: nn.Sequential
+        Layers before the split point.
+    h: nn.Sequential
+        Layers after the split point.
+    """
+
+    layers = list(model.named_children())
+
+    if id == None:
+        id = len(layers)-1
+    
+    g = nn.Sequential(*layers[:id])
+    h = nn.Sequential(*layers[id:])
+
+    return g, h
+
