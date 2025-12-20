67d8dd61fe43a0f012788b5ea943246a7e6e439b
maegonz <manutonymanu@gmail.com>
2025-12-20 23:27:56 +0100

    Functional residual net

--- model/resnet.py
+++ model/resnet.py
@@ -2,23 +2,27 @@ import torch
 import torch.nn as nn
 from model.resblock import ResBlock
 
-class ResNet(nn.Module):
-    def __init__(self, n_class : int):
-        super(ResNet, self).__init__()
+
+class ResNet18(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ResNet18, self).__init__()
         self.in_channels = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        
-        self.block1 = self._make_layer(ResBlock, 64, 2, stride=1)
-        self.block2 = self._make_layer(ResBlock, 128, 2, stride=2)
-        self.block3 = self._make_layer(ResBlock, 256, 2, stride=2)
-        self.block4 = self._make_layer(ResBlock, 512, 2, stride=2)
-        
+
+        self.layer1 = self._make_layer(ResBlock, 64, 2, stride=1)
+        self.layer2 = self._make_layer(ResBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(ResBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(ResBlock, 512, 2, stride=2)
+
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.flatten = nn.Flatten()
-        self.dense = nn.Linear(512, n_class)
+        self.flatten =  nn.Flatten()
+        self.fc = nn.Linear(512, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, ResBlock):
+                nn.init.constant_(m.bn2.weight, 0)
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -29,96 +33,16 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        y = self.conv1(x)
-        y = self.bn1(y)
-        y = self.relu(y)
-        y = self.maxpool(y)
-        
-        y = self.block1(y)
-        y = self.block2(y)
-        y = self.block3(y)
-        y = self.block4(y)
-        
-        y = self.avgpool(y)
-        y = self.flatten(y) 
-        y = self.dense(y)
-        return y
-    
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-# class ResNet18(nn.Module):
-#     def __init__(self):
-#         super(ResNet18, self).__init__()
-        
-#         self.conv1 = nn.Conv2d(
-#             in_channels = 3, out_channels = 64,
-#             kernel_size = 3, padding = 1,
-#             stride = 1, bias = False
-#         )
-#         self.bn1 = nn.BatchNorm2d(num_features = 64)
-        
-#         self.resblock1 = ResNet_Block(
-#             num_inp_channels = 64, num_channels = 64,
-#             stride = 1, dropout = 0.2,
-#             use_1x1_conv = False
-#         )
-        
-#         self.resblock2 = ResNet_Block(
-#             num_inp_channels = 64, num_channels = 64,
-#             stride = 1, dropout = 0.2,
-#             use_1x1_conv = False
-#         )
-        
-#         # Downsample-
-#         self.resblock3 = ResNet_Block(
-#             num_inp_channels = 64, num_channels = 128,
-#             stride = 2, dropout = 0.2,
-#             use_1x1_conv = True
-#         )
-        
-#         self.resblock4 = ResNet_Block(
-#             num_inp_channels = 128, num_channels = 128,
-#             stride = 1, dropout = 0.2,
-#             use_1x1_conv = False
-#         )
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
 
-#         # Downsample-
-#         self.resblock5 = ResNet_Block(
-#             num_inp_channels = 128, num_channels = 256,
-#             stride = 2, dropout = 0.2,
-#             use_1x1_conv = True
-#         )
-
-#         self.resblock6 = ResNet_Block(
-#             num_inp_channels = 256, num_channels = 256,
-#             stride = 1, dropout = 0.2,
-#             use_1x1_conv = False
-#         )
-
-#         # Downsample-
-#         self.resblock7 = ResNet_Block(
-#             num_inp_channels = 256, num_channels = 512,
-#             stride = 2, dropout = 0.2,
-#             use_1x1_conv = True
-#         )
-
-#         self.resblock8 = ResNet_Block(
-#             num_inp_channels = 512, num_channels = 512,
-#             stride = 1, dropout = 0.2,
-#             use_1x1_conv = False
-#         )
-        
-#         self.avg_pool = nn.AvgPool2d(kernel_size = 3, stride = 2)
-        
-    
-#     def forward(self, x):
-#         x = F.relu(self.bn1(self.conv1(x)))
-#         x = self.resblock1(x)
-#         x = self.resblock2(x)
-#         x = self.resblock3(x)
-#         x = self.resblock4(x)
-#         x = self.resblock5(x)
-#         x = self.resblock6(x)
-#         x = self.resblock7(x)
-#         x = self.resblock8(x)
-#         x = self.avg_pool(x).squeeze()
-#         return x
+        out = self.avgpool(out)
+        out = self.flatten(out)
+        out = self.fc(out)
+        return out
